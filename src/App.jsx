854002d833b27f16f7076b633af4ad0ca80f053c
LinkonJT <linkontripura@gmail.com>
2025-07-13 20:28:49 +0600

    istalled tailwind flowbite dotenv

---
+++ src/App.jsx
@@ -0,0 +1,25 @@
+
+import './App.css'
+import { Button } from 'flowbite-react'
+import NavbarA from './component/Navbar'
+
+
+
+function App() {
+
+  return (
+    <>
+ 
+      <h1>TEEEEST</h1>
+     
+
+
+      <Button>Click me</Button>;
+      <NavbarA></NavbarA>
+
+      
+    </>
+  )
+}
+
+export default App
